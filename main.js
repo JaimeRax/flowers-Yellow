71c01a14765b78f65cafde7bd8ef0c1a73090160
JaimeRax <jaimecaal60@gmail.com>
2024-09-21 17:09:03 -0600

    fix(music): add music

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+ window.addEventListener('load', function() {
+            const audio = document.getElementById('flowerMusic');
+            audio.play().catch(function(error) {
+                console.log("Reproducción automática bloqueada, por favor interactúa con la página.");
+            });
+        });
